2e79dcb54abf3dd9684c45af23fdb7ff441da09e
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2026-01-02 20:06:17 +0100

    Cross platform NetCore interop. (for ShaderCompiler, rest is injected)

--- engine/Sandbox.Engine/Core/Interop/NetCore.cs
+++ engine/Sandbox.Engine/Core/Interop/NetCore.cs
@@ -3,7 +3,7 @@
 	/// <summary>
 	/// Interop will try to load dlls from this path, e.g bin/win64/
 	/// </summary>
-	internal static string NativeDllPath { get; set; } = "bin/win64/";
+	internal static string NativeDllPath { get; set; }= string.Empty;
 
 	/// <summary>
 	/// From here we'll open the native dlls and inject our function pointers into them,
@@ -15,6 +15,30 @@
 		// of the managed system to work with - since they can all assume CurrentDirectory is
 		// where you would expect it to be instead of in the fucking bin folder.
 		System.Environment.CurrentDirectory = gameFolder;
+
+		//Unfortunately I have to duplicate logic from launcher.
+		//The reason being in most cases NativeDllPath is injected from Sandbox.LaunchEnvironment,
+		//but ShaderCompiler uses that property directly.
+		if(string.IsNullOrEmpty(NativeDllPath))
+		{
+			if(OperatingSystem.IsWindows())
+			{
+				NativeDllPath = "bin/win64";
+			} 
+			//Absolute paths for these two.
+			else if (OperatingSystem.IsLinux())
+			{
+				NativeDllPath = $"{gameFolder}/bin/linuxsteamrt64";
+			}
+			else if (OperatingSystem.IsMacOS())
+			{
+				//TODO: Also check for architecture here.
+				NativeDllPath = $"{gameFolder}/bin/osxarm64";
+			} else
+			{
+				throw new Exception("Unsupported platform for interop.");
+			}
+		}
 
 		// engine is always initialized
 		Managed.SandboxEngine.NativeInterop.Initialize();
