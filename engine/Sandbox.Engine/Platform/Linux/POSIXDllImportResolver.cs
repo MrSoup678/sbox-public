a4f1fde3f827d4bba5c581bfe4ffcfde630a71da
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2026-01-24 13:05:13 +0100

    Add Handle caching.

--- engine/Sandbox.Engine/Platform/Linux/POSIXDllImportResolver.cs
+++ engine/Sandbox.Engine/Platform/Linux/POSIXDllImportResolver.cs
@@ -9,44 +9,63 @@ namespace Sandbox;
 
 public class SboxNativesResolver
 {
-    public static void SetupResolvers()
-    {
-        Assembly skiaSharpAssebmlyRef = typeof(SKAlphaType).Assembly, harfBuzzSharpAssemblyRef = typeof(HarfBuzzSharp.Font).Assembly;
-        NativeLibrary.SetDllImportResolver(skiaSharpAssebmlyRef,SkiaSharpImportResolver);
-        NativeLibrary.SetDllImportResolver(harfBuzzSharpAssemblyRef,HarfBuzzSharpImportResolver);
-    }
+	private static Dictionary<string,IntPtr> cachedLibHandles = new Dictionary<string, IntPtr>();
+	public static void SetupResolvers()
+	{
+		Assembly skiaSharpAssebmlyRef = typeof( SKAlphaType ).Assembly, harfBuzzSharpAssemblyRef = typeof( HarfBuzzSharp.Font ).Assembly;
+		NativeLibrary.SetDllImportResolver( skiaSharpAssebmlyRef, SkiaSharpImportResolver );
+		NativeLibrary.SetDllImportResolver( harfBuzzSharpAssemblyRef, HarfBuzzSharpImportResolver );
+	}
 
 //TODO: Rename these files.
 	private static IntPtr HarfBuzzSharpImportResolver( string libraryName, Assembly assembly, DllImportSearchPath? searchPath )
 	{
-        if (libraryName == "libHarfBuzzSharp")
-        {
-            if(OperatingSystem.IsLinux())
-            {
-                return NativeLibrary.Load($"{NetCore.NativeDllPath}/libHarfBuzzSharp.so.0.60830.0");
-            } else if (OperatingSystem.IsMacOS())
-            {
-                
-            return NativeLibrary.Load($"{NetCore.NativeDllPath}/libHarfBuzzSharp.dylib");
-            }
-        }
+		if ( libraryName == "libHarfBuzzSharp" )
+		{
+			if ( cachedLibHandles.ContainsKey( libraryName ) )
+			{
+				return cachedLibHandles[libraryName];
+			}
+			IntPtr libHandle;
+			if ( OperatingSystem.IsLinux() )
+			{
+				libHandle =  NativeLibrary.Load( $"{NetCore.NativeDllPath}/libHarfBuzzSharp.so.0.60830.0" );
+				cachedLibHandles.Add(libraryName,libHandle);
+				return libHandle;
+			}
+			else if ( OperatingSystem.IsMacOS() )
+			{
+				libHandle =  NativeLibrary.Load( $"{NetCore.NativeDllPath}/libHarfBuzzSharp.dylib" );
+				cachedLibHandles.Add(libraryName,libHandle);
+				return libHandle;
+			}
+		}
 		return IntPtr.Zero;
 	}
 
 
 	private static IntPtr SkiaSharpImportResolver( string libraryName, Assembly assembly, DllImportSearchPath? searchPath )
 	{
-        if (libraryName == "libSkiaSharp")
-        {
-            if(OperatingSystem.IsLinux())
-            {
-            return NativeLibrary.Load($"{NetCore.NativeDllPath}/libSkiaSharp.so.116.0.0");
-            } else if (OperatingSystem.IsMacOS())
-            {
-                
-            return NativeLibrary.Load($"{NetCore.NativeDllPath}/libSkiaSharp.dylib");
-            }
-        }
+		if ( libraryName == "libSkiaSharp" )
+		{
+			if ( cachedLibHandles.ContainsKey( libraryName ) )
+			{
+				return cachedLibHandles[libraryName];
+			}
+			IntPtr libHandle;
+			if ( OperatingSystem.IsLinux() )
+			{
+				libHandle = NativeLibrary.Load( $"{NetCore.NativeDllPath}/libSkiaSharp.so.116.0.0" );
+				cachedLibHandles.Add(libraryName,libHandle);
+				return libHandle;
+			}
+			else if ( OperatingSystem.IsMacOS() )
+			{
+				libHandle = NativeLibrary.Load( $"{NetCore.NativeDllPath}/libSkiaSharp.dylib" );
+				cachedLibHandles.Add(libraryName,libHandle);
+				return libHandle;
+			}
+		}
 		return IntPtr.Zero;
 	}
 
